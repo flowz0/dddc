3fdf815ff33579d318ec925a3354f46206f44f4d
flowz0 <billylflowers@gmail.com>
2024-12-01 16:54:54 -0800

    Updated title and icon

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'https://imgur.com/',
-              port: '',
-              pathname: '/**',
-              search: '',
-            },
-          ],
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
