25ebb8cd8f999be614fe081e7b420f0f65f3e4a2
flowz0 <billylflowers@gmail.com>
2024-12-03 14:31:03 -0800

    Debugged 'failed to compile' error Updated imports for components

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import BeforeAfter from "./_components/BeforeAfter";
-import Contact from "./_components/Contact";
-import Footer from "./_components/Footer";
-import FAQ from "./_components/FAQ";
-import Hero from "./_components/Hero";
-import Services from "./_components/Services";
-import SpecialOffers from "./_components/SpecialOffers";
+import BeforeAfter from "./components/BeforeAfter";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import FAQ from "./components/FAQ";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import SpecialOffers from "./components/SpecialOffers";
 
 export default function Home() {
   return (
