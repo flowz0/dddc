c70632e8a1c94da9b97d79d63194c101712a56ba
flowz0 <billylflowers@gmail.com>
2024-12-07 22:44:51 -0800

    Improved AccordionFAQ.jsx Reordered FAQ and added defaultExpand to first FAQ

--- app/components/AccordionFAQ.jsx
+++ app/components/AccordionFAQ.jsx
@@ -4,9 +4,9 @@ import { Accordion, AccordionItem } from "@nextui-org/accordion";
 
 export default function AccordionFAQ() {
 	const answer =
-		"Typically, every 2-5 years, depending on home factors like pets, recent renovations, or health issues.";
-	const answer2 =
 		"Look for excess dust, unexplained allergies, or musty odors.";
+	const answer2 =
+		"Typically, every 2-5 years, depending on home factors like pets, recent renovations, or health issues.";
 	const answer3 =
 		"We use a Negative Air Machine for a thorough cleaning and offer optional sanitization.";
 
@@ -17,14 +17,14 @@ export default function AccordionFAQ() {
 	}
 
 	return (
-		<Accordion selectionMode="multiple" showDivider={false} className="text-[#333333] flex flex-col gap-y-4" itemClasses={itemClasses}>
-			<AccordionItem key="1" aria-label="How often should air ducts be cleaned?" title="How often should air ducts be cleaned?">
-				<div>
-					{answer}
-				</div>
+		<Accordion selectionMode="multiple" defaultExpandedKeys={["1"]} showDivider={false} className="text-[#333333] flex flex-col gap-y-4" itemClasses={itemClasses}>
+			<AccordionItem key="1" aria-label="How do I know if my ducts need cleaning?" title="How do I know if my ducts need cleaning?">
+				{answer}
 			</AccordionItem>
-			<AccordionItem key="2" aria-label="How do I know if my ducts need cleaning?" title="How do I know if my ducts need cleaning?">
-				{answer2}
+			<AccordionItem key="2" aria-label="How often should air ducts be cleaned?" title="How often should air ducts be cleaned?">
+				<div>
+					{answer2}
+				</div>
 			</AccordionItem>
 			<AccordionItem key="3" aria-label="What methods do you use?" title="What methods do you use?">
 				{answer3}
